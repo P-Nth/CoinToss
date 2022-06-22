0396bd026283852d4ba4a62f931351892184dfb5
P-Nth <philemonondigo@protonmail.com>
2022-06-22 23:50:27 +0300

    Created Functions

--- assets/main.js
+++ assets/main.js
@@ -1,66 +1,79 @@
-function coinToss() {
+document.getElementById("head").addEventListener("click", (e) => {
+  document.getElementById("userChoice").innerHTML =
+    document.getElementById("head").innerHTML;
+});
+document.getElementById("tail").addEventListener("click", (e) => {
+  document.getElementById("userChoice").innerHTML =
+    document.getElementById("tail").innerHTML;
+});
+
+function compChoice(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function theAnswer(head, tail) {
+  return Math.floor(Math.random() * (tail - head)) + head;
+}
+
+function findWinner() {
   let userCount = 0;
   let compCount = 0;
-  for (let i = 0; i < 6; i++) {
-    function lop() {
-      //   while (i < 6) {
-      // user choice
-      document.getElementById("head").addEventListener("click", (e) => {
-        document.getElementById("userChoice").innerHTML =
-          document.getElementById("head").innerHTML;
-      });
-      document.getElementById("tail").addEventListener("click", (e) => {
-        document.getElementById("userChoice").innerHTML =
-          document.getElementById("tail").innerHTML;
-      });
-      useranswer = document.getElementById("userChoice").innerHTML;
-      // Computer Choice
-      function compChoice(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      }
-      let cChoice = compChoice(1, 3);
-      switch (cChoice) {
-        case 1:
-          comp = "Head";
-          break;
-        case 2:
-          comp = "Tail";
-          break;
-      }
-      document.getElementById("compChoice").innerHTML = comp;
-      // Correct Answer
-      function theAnswer(head, tail) {
-        return Math.floor(Math.random() * (tail - head)) + head;
-      }
-      let ans = theAnswer(5, 7);
-      switch (ans) {
-        case 5:
-          answer = "Head";
-          break;
-        case 6:
-          answer = "Tail";
-          break;
-      }
-      document.getElementById("answer").innerHTML = answer;
-      //   calculate winner
-      if (useranswer === answer) {
-        userCount += 1;
-      }
-      if (comp === answer) {
-        compCount += 1;
-      }
-      console.log(userCount, compCount);
-      return userCount, compCount;
+  if (useranswer !== answer) {
+    if (useranswer < compChoice) {
+      compCount += 1;
+    } else if (useranswer > compChoice) {
+      userCount += 1;
     }
-    // return lop();
-  }
-  if (lop.userCount > lop.compCount) {
-    document.getElementById("winner").innerHTML = "You win!";
-  } else if (lop.userCount < lop.compCount) {
-    document.getElementById("winner").innerHTML = "You loose!";
   } else {
-    document.getElementById("winner").innerHTML = "A tie";
+    userCount += 0;
+    compCount += 0;
   }
-  return lop();
+
+  if (userCount > compCount) {
+    document.getElementById("winner").innerHTML = "You Win!";
+  } else if (userCount < compCount) {
+    document.getElementById("winner").innerHTML = "You Loose!";
+  } else {
+    document.getElementById("winner").innerHTML = "Its A Tie!";
+  }
+  console.log(userCount, compCount);
+  return userCount, compCount;
 }
-// }
+
+function guess() {
+  // user choice
+  useranswer = document.getElementById("userChoice").innerHTML;
+  // Computer Choice
+  compChoice();
+  let cChoice = compChoice(1, 3);
+  switch (cChoice) {
+    case 1:
+      comp = "Head";
+      break;
+    case 2:
+      comp = "Tail";
+      break;
+  }
+  document.getElementById("compChoice").innerHTML = comp;
+  // Correct Answer
+  theAnswer();
+  let ans = theAnswer(2, 4);
+  switch (ans) {
+    case 2:
+      answer = "Head";
+      break;
+    case 4:
+      answer = "Tail";
+      break;
+  }
+  document.getElementById("answer").innerHTML = answer;
+
+  return useranswer, compChoice, theAnswer;
+}
+
+function coinToss() {
+  guess();
+  //   calculate winner
+  findWinner();
+}
+// alert("Hello World!");
